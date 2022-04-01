efb835ed87b1de522c89e44535597795f79d2d5e
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-04-01 14:52:20 -0700

    DONE

--- Module3Assignments/src/fibonacci/Main.java
+++ Module3Assignments/src/fibonacci/Main.java
@@ -2,15 +2,18 @@ package fibonacci;
 
 public class Main {
     public static void main(String[] args) {
+        //the first two numbers of the fibonacci sequence is 1 and 1
         final int FIB_PAIR = 1;
+        //the sum starts at 1 and grows as N increases
         final int FIB_SUM_INIT = 1;
 
 
-        System.out.print(fibonacci(FIB_PAIR,FIB_PAIR,2,FIB_SUM_INIT));
+        //set limit (third param) to N
+        System.out.print(fibonacci(FIB_PAIR,FIB_PAIR, 6,FIB_SUM_INIT));
 
     }
 
-
+//does this count as 2 lines? LOL
     public static int fibonacci(int a, int b, int limit, int sum){
         if(limit > 1) return (fibonacci(b, a+b, limit-1, sum+b));
         else return (sum);
