9e669963dac0419685bc2de3214fcf05c45e31cb
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-04-01 15:02:33 -0700

    awdawd
    awdawdw

--- Module3Assignments/src/fibonacci/Main.java
+++ Module3Assignments/src/fibonacci/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //the first two numbers of the fibonacci sequence is 1 and 1
         final int FIB_PAIR = 1;
+
         //the sum starts at 1 and grows as N increases
         final int FIB_SUM_INIT = 1;
 
-
-        //set limit (third param) to N
-        System.out.print(fibonacci(FIB_PAIR,FIB_PAIR, 6,FIB_SUM_INIT));
+        //set limit (third param) to N                 DOWN HERE
+        System.out.print(fibonacci(FIB_PAIR,FIB_PAIR, 10,FIB_SUM_INIT));
 
     }
 
